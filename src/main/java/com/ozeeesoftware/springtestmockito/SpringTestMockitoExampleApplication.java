f4ee0a7c4a83ca5fb4fe1d3ab01d17ddb93be5c7
ozankocali <ozan.kocali@hotmail.com>
2021-05-31 13:45:02 +0300

    Initial commit-User model and repository added

---
+++ src/main/java/com/ozeeesoftware/springtestmockito/SpringTestMockitoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ozeeesoftware.springtestmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestMockitoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestMockitoExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ozeeesoftware/springtestmockito/model/User.java
@@ -0,0 +1,32 @@
+package com.ozeeesoftware.springtestmockito.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+
+}
